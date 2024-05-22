bf6cc21b5ae570fda3d294c08fc9cc40b0201a6c
Dennis Agbo <dennisjr4557@gmail.com>
2024-05-22 13:14:39 +0100

    cors little update

--- backend/app.js
+++ backend/app.js
@@ -12,35 +12,39 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
-// Define allowed origins
-const allowedOrigins = [
-  'http://localhost:5173', // Local development URL
-  'https://medconnect-pi.vercel.app',
-  'https://medconnect-pi.vercel.app/',
-  'https://medconnect-7d0l21meg-medconnect-edu-ng.vercel.app/' // Production frontend URL
-];
+// // Define allowed origins
+// const allowedOrigins = [
+//   'http://localhost:5173', // Local development URL
+//   'https://medconnect-pi.vercel.app',
+//   'https://medconnect-pi.vercel.app/',
+//   'https://medconnect-7d0l21meg-medconnect-edu-ng.vercel.app/' // Production frontend URL
+// ];
 
-// CORS configuration
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-  credentials: true, // Allow cookies and other credentials to be sent
-  allowedHeaders: ["Content-Type", "Authorization"], // Add other headers as needed
+// // // CORS configuration
+// // const corsOptions = {
+// //   origin: function (origin, callback) {
+// //     // Allow requests with no origin (like mobile apps, curl requests)
+// //     if (!origin) return callback(null, true);
+// //     if (allowedOrigins.indexOf(origin) !== -1) {
+// //       callback(null, true);
+// //     } else {
+// //       callback(new Error('Not allowed by CORS'));
+// //     }
+// //   },
+// //   methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+// //   credentials: true, // Allow cookies and other credentials to be sent
+// //   allowedHeaders: ["Content-Type", "Authorization"], // Add other headers as needed
   
-  preflightContinue: false,
-  optionsSuccessStatus: 204 // Ensure preflight requests return 204
-};
+// //   preflightContinue: false,
+// //   optionsSuccessStatus: 204 // Ensure preflight requests return 204
+// // };
 
 // Apply CORS middleware
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://medconnect-7d0l21meg-medconnect-edu-ng.vercel.app', // Your frontend URL
+  methods: ['GET', 'POST'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
 
 app.use(cookieParser());
 app.use(express.json());
