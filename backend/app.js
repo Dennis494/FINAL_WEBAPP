385986a1766cbe31ff0d3de6c7ae7f32c55d32ea
Dennis Agbo <dennisjr4557@gmail.com>
2024-05-22 08:47:05 +0100

    Updated app.js with Correct CORS Configuration

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,27 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
+// Define allowed origins
+const allowedOrigins = [
+  'http://localhost:5173', // Local development URL
+  'https://medconnect-pi.vercel.app/' // Production frontend URL
+];
+
+// Update CORS configuration
 app.use(
   cors({
-    origin: '*',
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  }) 
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true, // Allow cookies and other credentials to be sent
+  })
 );
 
 app.use(cookieParser());
